3b692dd5790e61857940c4e352593f93d548c7ea
Richard Yee <richtomyee@gmail.com>
2017-06-11 23:15:01 -0400

    firstt up

---
+++ script.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+$('.logo').css('height', window.innerHeight+'px');
+
+ $.getJSON("database.json", function(json) {
+   var container = $("<div class='eventcontainer'></div>");
+  //  var myList = JSON.parse(json.speakers);
+   var jsonLength = Object.keys(json.speakers).length
+   var i = 0;
+///loop for initial load
+   for (i; i < jsonLength; i++) {
+///////gets json data
+      var speakerID = "speaker"+i;
+      var speakerMonth = json.speakers[speakerID].month;
+      var speakerDate = json.speakers[speakerID].date;
+      var speakerName = json.speakers[speakerID].name;
+      var speakerDesc = json.speakers[speakerID].description;
+      var speakerImg = json.speakers[speakerID].photo;
+      console.log(speakerDesc);
+//////component variables
+      var speaker = $('<div class="speaker" id="'+speakerID+'"></div>');
+      var month = $('<div class="month">'+speakerMonth+'</div>');
+      var date = $('<div class="date">'+speakerDate+'</div>');
+      var name = $('<div class="name">'+speakerName+'</div>');
+      var desc = $('<div class="description">'+speakerDesc+'</div>');
+      var photo = $('<div class="photo" style="width:50%;background-image:url('+speakerImg+')"></div>');
+
+      var title = $('<div class="title"></div>');
+      var copy = $('<div class="copy"></div>');
+      title.append(month,date,name);
+      copy.append(desc,photo);
+      speaker.append(title,copy);
+      container.append(speaker);
+
+//////expand function
+
+      speaker.click(function() {
+       $('.selected').not($(this)).removeClass("selected");
+       $(this).toggleClass("selected");
+     });
+
+   }
+
+   console.log(jsonLength);
+   $('.info').html(container);
+
+ });
+
+
+
+
+
+
+
+
+
+});
